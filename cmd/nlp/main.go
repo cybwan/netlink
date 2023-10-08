993080033d63f72395a6c74817a55d8ea7bacd70
Cybwan <baili@flomesh.io>
2023-10-08 11:51:19 +0000

    init

--- cmd/nlp/main.go
+++ cmd/nlp/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"reflect"
 
+	nl "github.com/cybwan/netlink/pkg/netlink"
+)
+
+func main() {
+	links, err := nl.LinkList()
+	if err != nil {
+		return
+	}
+	for _, link := range links {
+		lt := reflect.TypeOf(link)
+		fmt.Println(lt)
+	}
 }
